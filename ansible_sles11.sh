a7e7a5145c440f43305b464a165e79948c980ddc
Kamil Monticolo <kmonticolo+github@gmail.com>
2017-02-06 16:06:21 +0100

    Update ansible_sles11.sh

--- ansible_sles11.sh
+++ ansible_sles11.sh
@@ -12,11 +12,14 @@ sudo zypper --no-gpg-checks refresh || exit 3
 sudo zypper rm -y *python* || exit 4
 # sudo zypper rm -y dbus-1-python libxml2-python python-satsolver rpm-python python-xml python-base
 sudo zypper in -y python-devel libffi-devel || exit 5
-openssl version|grep -q 0.9 && wget -q https://www.openssl.org/source/openssl-1.0.2k.tar.gz -O -|tar zxf -
-cd openssl* || exit 6
-/usr/bin/perl ./Configure linux-x86_64 --openssldir=/usr -Wa,--noexecstack -fPIC || exit 7 # -fPIC is important
-make && sudo make install || exit 8
-cd .. ; wget -q https://bootstrap.pypa.io/ez_setup.py -O - | sudo python || exit 9
+# OpenSSL RPMS were created to speed up whole process. If you want co compile, uncomment below lines.
+sudo zypper in -y https://github.com/kmonticolo/Ansible-on-SLES11/blob/master/openssl-1.0.2k-1.x86_64.rpm \
+https://github.com/kmonticolo/Ansible-on-SLES11/blob/master/openssl-doc-1.0.2k-1.x86_64.rpm || exit 6
+#openssl version|grep -q 0.9 && wget -q https://www.openssl.org/source/openssl-1.0.2k.tar.gz -O -|tar zxf -
+#cd openssl* || exit 6
+#/usr/bin/perl ./Configure linux-x86_64 --openssldir=/usr -Wa,--noexecstack -fPIC || exit 7 # -fPIC is important
+#make && sudo make install || exit 8
+wget -q https://bootstrap.pypa.io/ez_setup.py -O - | sudo python || exit 9
 openssl version|grep -q 1.0 && sudo easy_install ansible && ansible --version || exit 10
 STOP=$(date +%s)
 echo "It tooks $(expr $STOP - $START ) seconds."
